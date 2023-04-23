2834f3a444f1c03458fa71a88ff6b3552c4c1d5d
Abdelrahman El-ShaiKH <88721486+Abdo73873@users.noreply.github.com>
2023-04-23 02:46:45 +0200

    Update user_layout.dart

--- lib/layout/users/user_layout.dart
+++ lib/layout/users/user_layout.dart
@@ -21,40 +21,13 @@ class UsersLayout extends StatelessWidget {
             children: [
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 30.0,vertical: 6.0),
-                child: SafeArea(
-                  child: GNav(
-                    rippleColor: secondaryColor,
-                    hoverColor: defaultColor,
-                    gap: 6,
-                    activeColor: defaultColor,
-                    padding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
-                    duration: Duration(milliseconds: 400),
-                    tabBackgroundColor: secondaryColor.withOpacity(.1),
-                    selectedIndex:cubit.currentIndex,
-                    onTabChange: (index){
-                      cubit.changeUsersBottomScreen(index);
-                    },
+                child:    //private code here
+
                     tabs: [
                       GButton(icon: Icons.family_restroom, text: 'All',leading:Icon( Icons.family_restroom,) ,),
                       GButton(icon: Icons.person, text: 'Friends',leading: Icon(Icons.person),),
                       GButton(icon: Icons.person_add_alt_1, text: 'Request',
-                          leading:Stack(
-                            alignment: AlignmentDirectional.topEnd,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(11.0),
-                            child: Icon( Icons.person_add,),
-                          ),
-                          CircleAvatar(
-                            radius: 10,
-                            child: Text('${cubit.requestsUid.length}',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 12.0,
-                              ),),
-                          ),
-                        ],
-                      )),
+                            //private code here
 
                     ],
                   ),
